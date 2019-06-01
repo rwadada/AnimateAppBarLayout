eada0bb3da85c93b0ee667f1d1c00c63987b4389
rwadada <wadada.knct@gmail.com>
2019-06-01 23:31:46 +0900

    update animate appbar layout logging expand

--- animateappbarlayout/src/main/java/com/rwadada/animateappbarlayout/AnimateAppBarLayout.kt
+++ animateappbarlayout/src/main/java/com/rwadada/animateappbarlayout/AnimateAppBarLayout.kt
@@ -1,2 +1,45 @@
 package com.rwadada.animateappbarlayout
 
+import android.content.Context
+import android.util.AttributeSet
+import android.util.Log
+import android.view.View
+import com.google.android.material.appbar.AppBarLayout
+
+class AnimateAppBarLayout(context: Context, attrs: AttributeSet) : AppBarLayout(context, attrs) {
+    companion object {
+        val TAG = AnimateAppBarLayout::class.java.simpleName
+    }
+
+    var targetLayoutId: Int? = null
+
+    private var targetLayoutHeight: Int = 0
+    private var targetLayoutWidth: Int = 0
+
+    fun addAppBarAnimation() {
+        addOnOffsetChangedListener(offsetChangedListener)
+    }
+
+    private val offsetChangedListener =
+        OnOffsetChangedListener { appBarLayout, verticalOffset ->
+            val targetLayout: View? = targetLayoutId?.let {
+                appBarLayout.findViewById(it)
+            }
+
+            if (targetLayout is View) {
+                when (verticalOffset) {
+                    0 -> {
+                        Log.d(TAG, "Expand")
+                        targetLayoutHeight = targetLayout.height
+                        targetLayoutWidth = targetLayout.width
+
+                        Log.d(TAG, "targetLayoutHeight : $targetLayoutHeight")
+                        Log.d(TAG, "targetLayoutWidth : $targetLayoutWidth")
+                    }
+                    else -> {
+
+                    }
+                }
+            }
+        }
+}
--- example/src/main/java/com/example/animateappbarlayout/MainActivity.kt
+++ example/src/main/java/com/example/animateappbarlayout/MainActivity.kt
@@ -2,11 +2,16 @@ package com.example.animateappbarlayout
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.rwadada.animateappbarlayout.AnimateAppBarLayout
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val appBarLayout: AnimateAppBarLayout = findViewById(R.id.appbar_layout)
+        appBarLayout.targetLayoutId = R.id.toolbar
+        appBarLayout.addAppBarAnimation()
     }
 }
