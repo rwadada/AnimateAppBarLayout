9814d73e5472ab7f801f8fa457ca8eaa256a8d0d
rwadada <wadada.knct@gmail.com>
2019-06-04 22:13:04 +0900

    update example

--- example/src/main/java/com/example/animateappbarlayout/MainActivity.kt
+++ example/src/main/java/com/example/animateappbarlayout/MainActivity.kt
@@ -14,7 +14,7 @@ class MainActivity : AppCompatActivity() {
 
         val appBarLayout: AnimateAppBarLayout = findViewById(R.id.appbar_layout)
 
-        val appBarScaleAnimation = AppBarScaleAnimation(
+        val appBarScaleAnimation1 = AppBarScaleAnimation(
             fromX = 1.0f,
             toX = 0.0f,
             fromY = 1.0f,
@@ -24,9 +24,23 @@ class MainActivity : AppCompatActivity() {
             pivotYType = Animation.RELATIVE_TO_SELF,
             pivotYVal = 0.5f
         )
-        val appBarAlphaAnimation = AppBarAlphaAnimation(
+        val appBarScaleAnimation2 = AppBarScaleAnimation(
+            fromX = 0.0f,
+            toX = 1.0f,
+            fromY = 0.0f,
+            toY = 1.0f,
+            pivotXType = Animation.RELATIVE_TO_SELF,
+            pivotXVal = 0.5f,
+            pivotYType = Animation.RELATIVE_TO_SELF,
+            pivotYVal = 0.5f
+        )
+        val appBarAlphaAnimation1 = AppBarAlphaAnimation(
             fromAlpha = 1.0f,
             toAlpha = 0.0f
+        )
+        val appBarAlphaAnimation2 = AppBarAlphaAnimation(
+            fromAlpha = 0.0f,
+            toAlpha = 1.0f
         )
         val appBarRotateAnimation = AppBarRotateAnimation(
             fromDegree = 0.0f,
@@ -40,29 +54,31 @@ class MainActivity : AppCompatActivity() {
             fromXType = Animation.RELATIVE_TO_PARENT,
             fromXValue = 0.0f,
             toXType = Animation.RELATIVE_TO_PARENT,
-            toXValue = 0.95f,
+            toXValue = -1.0f,
             fromYType = Animation.RELATIVE_TO_PARENT,
             fromYValue = 0.0f,
             toYType = Animation.RELATIVE_TO_PARENT,
-            toYValue = 1.0f
+            toYValue = 0.0f
         )
 
         val appBarAnimationSet1 = AppBarAnimationSet()
-        appBarAnimationSet1.addAnimation(appBarScaleAnimation)
-        appBarAnimationSet1.addAnimation(appBarAlphaAnimation)
+        appBarAnimationSet1.addAnimation(appBarScaleAnimation1)
+        appBarAnimationSet1.addAnimation(appBarAlphaAnimation1)
 
-        appBarLayout.setAnimation(appBarAnimationSet = appBarAnimationSet1,
-            targetResourceId = R.id.toolbar,
-            scrollViewResourceId = R.id.toolbar)
+        appBarLayout.setAnimation(appBarAnimationSet1, R.id.toolbar, R.id.toolbar)
 
         val appBarAnimationSet2 = AppBarAnimationSet()
-        appBarAnimationSet2.addAnimation(appBarScaleAnimation)
+        appBarAnimationSet2.addAnimation(appBarScaleAnimation2)
         appBarAnimationSet2.addAnimation(appBarRotateAnimation)
 
-        appBarLayout.setAnimation(appBarAnimationSet = appBarAnimationSet2,
-            targetResourceId = R.id.toolbar,
-            scrollViewResourceId = R.id.toolbar)
-
         appBarLayout.setAnimation(appBarAnimationSet2, R.id.image_view, R.id.toolbar)
+
+        val appBarAnimationSet3 = AppBarAnimationSet()
+        appBarAnimationSet3.addAnimation(appBarAlphaAnimation2)
+        appBarAnimationSet3.addAnimation(appBarScaleAnimation2)
+
+        appBarLayout.setAnimation(appBarAnimationSet3, R.id.text_view, R.id.toolbar)
+
+        appBarLayout.setAnimation(appBarTranslateAnimation, R.id.rocket, R.id.toolbar)
     }
 }
